1fd7324c056ad1525e7ce6d7d95deb7a9d459be5
codersunny812 <sengersunny448@gmail.com>
2023-12-13 13:03:50 +0530

    footer component

--- src/Componets/Footer/Footer.jsx
+++ src/Componets/Footer/Footer.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import india from '../../assets/India.png'
+import './responsive.css'
 
 const Footer = () => {
   return (
-    <div>This is the  foooter section</div>
+    <>
+    <div className="footer flex  flex-col items-center capitalize">
+        <h3 className=' text-center'>Copyright © 2023 LeetCode</h3>
+        <ul className='flex'>
+          <li className='px-2'>help centre|</li>
+          <li className='px-2'>students|</li>
+          <li className='px-2' >terms|</li>
+          <li className='px-2' >privacy policy</li>
+        </ul>
+        <div className="end flex justify-center items-center px-3 my-2">
+          <img src={india} alt="indian flag" className='rounded-full h-9 w-9' />
+          <h1 className='px-2'>India</h1>
+        </div>
+
+
+    </div>
+    </>
   )
 }
 
