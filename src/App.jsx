68d5d32a7162487f9de1788459094a198020d709
codersunny812 <sengersunny448@gmail.com>
2023-12-15 14:22:20 +0530

    lc clone

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
-// import Header from './Componets/Header/Header'
-// import Body from './Componets/Body/Body'
 import Footer from './Componets/Footer/Footer'
 import MainSection from './Componets/MainSection/MainSection'
+import {RouterProvider, createBrowserRouter} from 'react-router-dom'
+import Auth from './Auth/Auth'
+import Signup from './Componets/Auth/Signup'
+import Login from './Componets/Auth/Login'
+import ForgetPassowrd from './Componets/Auth/ForgetPassowrd'
 
 const App = () => {
   return (
@@ -11,9 +14,31 @@ const App = () => {
     <Body/> */}
     <MainSection/>
     <Footer/>
-
     </div>
   )
 }
 
-export default App
+const AppRouter = createBrowserRouter([
+  {
+    path:'/',
+    element:<App/>
+  },
+  {
+    path:'/auth',
+    element:<Auth/>
+  },
+  {
+     path:'/signUp',
+     element:<Signup/>
+  },
+  {
+    path: '/login',
+    element:<Login/>
+  },
+  {
+    path:'/forgotpassword',
+    element: <ForgetPassowrd />
+  }
+])
+
+export default AppRouter;
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
+import { RouterProvider } from 'react-router-dom'
+import AppRouter from './App.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <RouterProvider router={AppRouter}/>
     </ChakraProvider>
    
   </React.StrictMode>,
