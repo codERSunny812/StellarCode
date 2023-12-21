2608ccf5934d1b7e4de459e6152ddf266b9fe007
codersunny812 <sengersunny448@gmail.com>
2023-12-21 10:55:22 +0530

    main rendering component:

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Footer from './Componets/Footer/Footer'
 import MainSection from './Componets/MainSection/MainSection'
-import {RouterProvider, createBrowserRouter} from 'react-router-dom'
-import Auth from './Auth/Auth'
+import {Outlet, RouterProvider, createBrowserRouter} from 'react-router-dom'
 import Signup from './Componets/Auth/Signup'
 import Login from './Componets/Auth/Login'
 import ForgetPassowrd from './Componets/Auth/ForgetPassowrd'
+import Auth from './Componets/Auth/Auth'
 
 const App = () => {
   return (
@@ -21,10 +21,10 @@ const App = () => {
 const AppRouter = createBrowserRouter([
   {
     path:'/',
-    element:<App/>
+    element:<App/>,
   },
   {
-    path:'/auth',
+    path:'/status',
     element:<Auth/>
   },
   {
