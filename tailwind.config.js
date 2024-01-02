85f6e6ebfa0fd4afd0f8cdaf4f51f53a5aadf631
codersunny812 <sengersunny448@gmail.com>
2024-01-02 22:58:01 +0530

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ export default {
       fontFamily: {
         'poppins': ['Poppins', 'sans-serif']
       },
+      boxShadow:{
+        '3xl':'rgba(50, 50, 93, 0.25) 0px 30px 60px -12px, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px;'
+      }
     },
   },
   plugins: [],
