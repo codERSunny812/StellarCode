1acee05d0fb5137fd04b4644aec006d515414766
codersunny812 <sengersunny448@gmail.com>
2024-01-02 22:58:58 +0530

    constatn file

---
+++ src/constant.js
@@ -0,0 +1,8 @@
+export const Image1 = "https://assets.leetcode.com/users/images/770789b0-c96b-4663-86d1-baab25534864_1669795265.8012726.png";
+
+export const Image2 = "https://assets.leetcode.com/users/images/b0a08a5c-c575-48f6-9110-b6ae4e011e98_1655746322.579097.png";
+
+
+export const Image3 ="https://assets.leetcode.com/users/images/892c9a1b-5b04-43d0-b1d0-eef28171bd77_1702352272.10295.png";
+
+export const Image4 = "https://assets.leetcode.com/users/images/7b7c1fab-b877-47fa-bc72-548fbcc9f94a_1703037199.2794356.jpg";
