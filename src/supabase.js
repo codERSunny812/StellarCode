f76b84afac7644b2aa5e95056908fc17c12448ca
codersunny812 <sengersunny448@gmail.com>
2024-01-02 22:57:48 +0530

    supabase config

--- src/supabase.js
+++ src/supabase.js
@@ -1,5 +1,11 @@
 import {createClient} from '@supabase/supabase-js'
 
-const supabaseURL = "https://ejokulkdhyizpsdqoqmw.supabase.co"
-const supabaseKEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVqb2t1bGtkaHlpenBzZHFvcW13Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDMxMDI2NTYsImV4cCI6MjAxODY3ODY1Nn0.roANMN1iznXCa5sesq1uYpHj-D0mwwbvcd11PqrbbME";
- export const supabase = createClient(supabaseURL,supabaseKEY);
+
+const supabaseUrl ="https://ejokulkdhyizpsdqoqmw.supabase.co";
+
+// const supabaseKey = process.env.VITE_SUPABASE_KEY;
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVqb2t1bGtkaHlpenBzZHFvcW13Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDMxMDI2NTYsImV4cCI6MjAxODY3ODY1Nn0.roANMN1iznXCa5sesq1uYpHj-D0mwwbvcd11PqrbbME";
+
+export const supabase = createClient(supabaseUrl,supabaseKey);
+
+export const auth = supabase.auth;
