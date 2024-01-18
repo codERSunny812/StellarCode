7309756795f0e18dcd11b2ca66393f84fac278a7
codersunny812 <sengersunny448@gmail.com>
2024-01-19 01:55:40 +0530

    tailwind css updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,8 @@ export default {
         'poppins': ['Poppins', 'sans-serif']
       },
       boxShadow:{
-        '3xl':'rgba(50, 50, 93, 0.25) 0px 30px 60px -12px, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px;'
+        '3xl':'rgba(50, 50, 93, 0.25) 0px 30px 60px -12px, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px;',
+        '4xl':'rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;'
       }
     },
   },
