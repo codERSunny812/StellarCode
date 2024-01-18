7e03153f2243dd3fb909ee006c0c176e918baa26
codersunny812 <sengersunny448@gmail.com>
2024-01-19 01:54:13 +0530

    make this responsive

--- src/Componets/MainSection/MainSection.jsx
+++ src/Componets/MainSection/MainSection.jsx
@@ -2,17 +2,20 @@ import Logo from "../../assets/LeetCode_logo.png";
 import Lottie from "lottie-react";
 import ipad from "./ipad.json";
 import coding from "./coding.json";
-import codingScreen from "./codingScreen.json";
 import "../../index.css";
 import "./responsive.css";
 import { FaWpexplorer } from "react-icons/fa";
 import { MdDeveloperMode } from "react-icons/md";
 import { Link } from "react-router-dom";
+import { useState , useEffect } from "react";
+import CodeMirror from "@uiw/react-codemirror";
+import { vscodeDark } from "@uiw/codemirror-theme-vscode";
+
 
 const Section1 = () => {
   return (
     <>
-      <div className="mb-3 section1 flex p-16   justify-between">
+      <div className=" mb-3 section1 flex p-16   justify-between">
         <Lottie animationData={ipad} className="h-96 section1_animation" />
         <div className="section1_content text  flex flex-col  justify-center items-center capitalize">
           <h1 className="text-white font-bold h-[44px] w-[520px] text-center text-4xl font-poppins">
@@ -70,6 +73,88 @@ const Section2 = () => {
 };
 
 const Section3 = () => {
+
+  const myInitialCode = `public class Main {
+  public static void main(String[] args) {
+    if (20 > 18) {
+      System.out.println("20 is greater than 18"); // obviously
+    }
+  }
+}`;
+
+  const texts = [ `
+fn factorial(n: u64) -> u64 {
+    if n == 0 {
+        1
+    } else {
+        n * factorial(n - 1)
+    }
+}
+
+fn main() {
+    // Calculate and print the factorial of 5
+    let result = factorial(5);
+    println!("Factorial of 5 is: {}", result);
+}
+`, `package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+
+	fmt.Print("Enter a number: ")
+
+
+	var input string
+	fmt.Scanln(&input)
+
+	
+	num, err := strconv.Atoi(input)
+	if err != nil {
+		fmt.Println("Invalid input. Please enter a valid number.")
+		return
+	}
+	square := num * num
+	cube := num * num * num
+
+	fmt.Printf("Square of %d is: %d\n", num, square)
+	fmt.Printf("Cube of %d is: %d\n", num, cube)
+}
+`, `
+user_input = input("Enter a number: ")
+
+try:
+    
+    num = int(user_input)
+
+    
+    square = num ** 2
+    cube = num ** 3
+
+    print(f"Square of {num} is: {square}")
+    print(f"Cube of {num} is: {cube}")
+
+except ValueError:
+    
+    print("Invalid input. Please enter a valid number.")
+`];
+
+  const [code, setCode] = useState(myInitialCode);
+  const [textIndex, setTextIndex] = useState(0);
+
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTextIndex((prevIndex) => (prevIndex + 1) % texts.length);
+      setCode(texts[textIndex]);
+    }, 6000); // Change the interval time as needed (in milliseconds)
+
+    return () => clearInterval(intervalId); // Cleanup on component unmount
+  }, [textIndex, texts]);
+
   return (
     <>
       <div className=" section3 flex flex-col justify-center items-center bg-gray-200">
@@ -85,10 +170,17 @@ const Section3 = () => {
           </p>
         </div>
 
-        <Lottie
-          animationData={codingScreen}
-          className=" section3_animation w-[900px] h-[300px]"
-        />
+
+
+         <div className=" mb-6 mt-4">
+          <CodeMirror
+          theme={vscodeDark}
+          value={code}
+          height="300px"
+          width="600px"
+          />
+
+         </div>
       </div>
     </>
   );
@@ -97,7 +189,7 @@ const Section3 = () => {
 const MainSection = () => {
   return (
     <>
-      <div className="cont w-full  bg-gradient-to-br from-gray-900 via-gray-600 to-blue-gray-900">
+      <div className="cont w-full  bg-gradient-to-br from-gray-900 via-gray-600 to-blue-gray-900 ">
         <div className=" navbar flex flex-col md:flex-row justify-between">
           <div className="flex items-center capitalize">
             <Link to='/'>
