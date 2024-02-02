d19728ba1a9125002ee281d6ca528de3f7b0c659
codersunny812 <sengersunny448@gmail.com>
2024-02-02 20:46:52 +0530

    main redering component

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { ChakraProvider } from '@chakra-ui/react'
-import { RouterProvider } from 'react-router-dom'
-import AppRouter from './App.jsx'
-import { SessionContextProvider } from './Context/AuthContext'
-import { LangContextProvider } from './Context/LangContext'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import { RouterProvider } from "react-router-dom";
+import AppRouter from "./App.jsx";
+import { SessionContextProvider } from "./Context/AuthContext";
+import { LangContextProvider } from "./Context/LangContext";
 
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ChakraProvider>
-     
       <SessionContextProvider>
         <LangContextProvider>
           <RouterProvider router={AppRouter} />
         </LangContextProvider>
       </SessionContextProvider>
-     
     </ChakraProvider>
-   
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
