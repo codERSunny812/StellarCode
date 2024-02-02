f4b19c6b556bb2af28d4048729abda02e052c9a9
codersunny812 <sengersunny448@gmail.com>
2024-02-02 20:40:45 +0530

    app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,56 @@
-import Footer from './Componets/Footer/Footer'
-import MainSection from './Componets/MainSection/MainSection'
-import {createBrowserRouter} from 'react-router-dom'
-import Signup from './Componets/Auth/Signup'
-import Login from './Componets/Auth/Login'
-import ForgetPassowrd from './Componets/Auth/ForgetPassowrd'
-import Auth from './Componets/Auth/Auth'
-import Home from './Pages/Home'
-import Profile from './Utils/Profile'
-import ProblemsQuestion from './Utils/ProblemsQuestion'
-
-
-
-
+import Footer from "./Componets/Footer/Footer";
+import MainSection from "./Componets/MainSection/MainSection";
+import { createBrowserRouter } from "react-router-dom";
+import Signup from "./Componets/Auth/Signup";
+import Login from "./Componets/Auth/Login";
+import ForgetPassowrd from "./Componets/Auth/ForgetPassowrd";
+import Home from "./Pages/Home";
+import Profile from "./Utils/Profile";
+import ProblemsQuestion from "./Utils/ProblemsQuestion";
+import VerifyInfo from "./Pages/VerifyInfo";
 
 const App = () => {
   return (
     <div>
-    <MainSection/>
-  
-    <Footer/>
+      <MainSection />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
 const AppRouter = createBrowserRouter([
   {
-    path:'/',
-    element:<App/>,
+    path: "/",
+    element: <App />,
   },
   {
-    path:'/status',
-    element:<Auth/>
+    path: "/signUp",
+    element: <Signup />,
   },
   {
-     path:'/signUp',
-     element:<Signup/>
+    path: "/login",
+    element: <Login />,
   },
   {
-    path: '/login',
-    element:<Login/>
+    path: "/forgotpassword",
+    element: <ForgetPassowrd />,
   },
   {
-    path:'/forgotpassword',
-    element: <ForgetPassowrd />
+    path: "/problemset",
+    element: <Home />,
   },
   {
-   path:'/problemset',
-  //  element:<RouterProvider router={HomeRouter} /> 
-  element:<Home/>
+    path: "/profile",
+    element: <Profile />,
   },
   {
-    path:'/profile',
-    element:<Profile/>
+    path: "/problems/:id",
+    element: <ProblemsQuestion />,
   },
   {
-   path:'/problems/:id',
-   element:<ProblemsQuestion/>
-  }
-])
-
-
+    path: "/verifyuser",
+    element: <VerifyInfo />,
+  },
+]);
 
 export default AppRouter;
